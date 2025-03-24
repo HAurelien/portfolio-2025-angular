0bdacff7c13cddb188f58c0cc302e42a8aa902d6
Aurélien Habermacher <aurelienhabermacher@gmail.com>
2025-03-24 17:32:33 +0100

    word cloud

--- src/app/features/home/components/home/home.component.ts
+++ src/app/features/home/components/home/home.component.ts
@@ -12,39 +12,53 @@ export class HomeComponent {
 
   backgroundImageUrl = "images/background_home.jpg"
 
+  words: any[] = [
+    { text: 'C++', value: 35 },
+    { text: 'C#', value: 30 },
+    { text: 'C', value: 25 },
+    { text: 'Python', value: 40 },
+    { text: 'Java', value: 40 },
+    { text: 'Kotlin', value: 35 },
+    { text: 'SQL', value: 35 },
+    { text: 'TypeScript', value: 25 },
+
+    { text: 'AI', value: 30 },
+    { text: 'Angular', value: 25 },
+    { text: 'SpringBoot', value: 35 },
+    { text: 'Django', value: 20 },
+
+    { text: 'Curiosity', value: 40 },
+    { text: 'Communication', value: 35 },
+    { text: 'Adaptable', value: 35 },
+    { text: 'Passion', value: 40 },
+    { text: 'Resilience', value: 30 },
+    { text: 'Patience', value: 30},
+    { text: 'Responsibility', value: 30},
+
+    { text: 'Agile', value: 30 }, 
+    { text: 'Collaboration', value: 30 }, 
+    { text: 'Problem-Solving', value: 30 }, 
+    { text: 'Leadership', value: 30 }, 
+    { text: 'Time Management', value: 25 }, 
+
+    { text: 'Jira', value: 25 },
+    { text: 'Git', value: 25 },
+    { text: 'Unity', value: 20 },
+    { text: 'Blender', value: 20 },
+  ];
+
   skills : DataBlockModel[] = [
     {
       title: 'Software Development',
       description: 'My experience, mostly in OOP: from java server apps to python softwares or C# games',
       icon: 'icons/brain_cogwheel.png',
-      onClick: () => {this.router.navigateByUrl("home/skills/backend")}
+      onClick: () => {this.router.navigate(["/home", "skills"], {queryParams: {type: ["backend"]}})}
     },
     {
       title: 'Frontend Dev',
       description: 'My experience in developing frontend of my server apps',
-      icon: 'icons/angular.png',
-      onClick: () => {this.router.navigateByUrl("home/skills/frontend")}
-    },
-  ];
-
-  projects = [
-    {
-      title: 'Portfolio Website',
-      description: 'A personal portfolio website showcasing my projects and skills.',
-      image: 'images/project1.png',
-      link: 'https://github.com/HAurelien/portfolio-2025-angular',
-    },
-    {
-      title: 'Task Manager App',
-      description: 'A task management application built with Angular.',
-      image: 'images/project2.jpg',
-      link: 'https://github.com/your-username/task-manager',
-    },
-    {
-      title: 'E-commerce Website',
-      description: 'An e-commerce site built with Angular and Firebase.',
-      image: 'images/project3.png',
-      link: 'https://github.com/your-username/e-commerce',
+      icon: 'icons/front-end.png',
+      onClick: () => {this.router.navigate(["/home", "skills"], {queryParams: {type: ["frontend"]}})}
     },
   ];
 }
